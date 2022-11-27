47f2bf41571349bcddd618929ec8820c73114eb0
alexuschristensen <alexusdc6@gmail.com>
2022-11-26 19:47:15 -0800

    debugged completely, runs fine

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 mongoose.connect(
-    process.env.MONGODB_URI || 'mongodb+srv://user:PaSSWoRd@cluster0.ogkz6cn.mongodb.net/socialmedia',
+    process.env.MONGODB_URI || 'mongodb+srv://Bob1:Password1@cluster0.ogkz6cn.mongodb.net/socialmedia',
     {
       useNewUrlParser: true,
       useUnifiedTopology: true,
@@ -17,9 +17,6 @@ mongoose.connect(
   
   // Use this to log mongo queries being executed!
 mongoose.set('debug', true);
-  app.use(express.json());
-  app.use(express.urlencoded({ extended: true }));
-    
   app.use(require('./routes'));
   
   app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
